138b0542bc29a1141da9ad0a2283dff8403eb2ba
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2025-06-26 20:20:04 +0530

    switch cases

--- Main.java
+++ Main.java
@@ -4,9 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        System.out.println(name);
+       Scanner sc = new Scanner(System.in);
+       int num = sc.nextInt();
 
+        switch(num){
+            case 1 :
+                System.out.println("the number is 1");
+                break;
+            case 2 :
+                System.out.println("the number is 2");
+                break;
+
+            case 3 :
+                System.out.println("the number is 3");
+                break;
+            default:
+                System.out.println("error");
+        }
     }
 }
