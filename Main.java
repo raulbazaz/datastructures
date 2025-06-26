0aa133bae7a5c8b0fbf6b9a584af40a590dc3b14
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2025-06-26 20:58:49 +0530

    table

--- Main.java
+++ Main.java
@@ -3,23 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-       Scanner sc = new Scanner(System.in);
-       int num = sc.nextInt();
-
-        switch(num){
-            case 1 :
-                System.out.println("the number is 1");
-                break;
-            case 2 :
-                System.out.println("the number is 2");
-                break;
-
-            case 3 :
-                System.out.println("the number is 3");
-                break;
-            default:
-                System.out.println("error");
-        }
+     int table = 2;
+     for(int i=0;i<11;i++){
+         var final_table = table * i;
+         System.out.println(final_table);
+     }
     }
 }
