eefff762f93d764396ddafc45b3e987979d4bcd4
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2025-06-29 01:28:36 +0530

    factorial

--- Methods.java
+++ Methods.java
@@ -2,14 +2,26 @@ import java.util.Scanner;
 
 public class Methods {
 
-    public static int sumTwo(int a, int b){
+//    public static int sumTwo(int a, int b){
+//
+//        return a+b;
+//
+//    }
 
-        return a+b;
+    // finding the factorial of a number
 
+    public static void numFactorial(int n){
+        int factorial = 1;
+        for (int i=n ; i>=1;i--){
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
     }
 
+
     public static void main(String args[]){
-        int finalAnswer = sumTwo(1,2);
-        System.out.println(finalAnswer);
+//        int finalAnswer = sumTwo(1,2);
+//        System.out.println(finalAnswer);
+        numFactorial(5);
     }
 }
