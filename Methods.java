733c9f83dea0245e12bb3d2568c365fcd5a8bf5b
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2025-06-29 01:18:26 +0530

    methods and funcs

--- Methods.java
+++ Methods.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Methods {
 
-    public static void main(String args[]){
+    public static int sumTwo(int a, int b){
 
+        return a+b;
+
+    }
+
+    public static void main(String args[]){
+        int finalAnswer = sumTwo(1,2);
+        System.out.println(finalAnswer);
     }
 }
